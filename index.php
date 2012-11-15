a78872b3aaf7a2787b2cf65b4a97fa1e59999e61
Alex Pappas <pappasaj@gmail.com>
2012-11-14 19:54:44 -0500

    detelting jeremy

--- index.php
+++ index.php
@@ -13,5 +13,5 @@
  */
 define('WP_USE_THEMES', true);
 
-/** Loads the WordPress Environment and Template jeremy */
+/** Loads the WordPress Environment and Template */
 require('./wordpress/wp-blog-header.php');
